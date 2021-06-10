251e86e76a8a93ac679693e0e090e80673642fb5
gtcarroll <gtcarroll96@gmail.com>
2021-06-09 17:14:24 -0700

    v1.371 - bird image preloading disabled

--- src/components/birdfeed/BirdFeed.js
+++ src/components/birdfeed/BirdFeed.js
@@ -19,12 +19,12 @@ export const BirdFeed = (props) => {
   let forwardToggle = !props.isLastTweet;
   let resetToggle = props.isLoaded;
 
-  useEffect(() => {
-    const images = [bird, birdEureka, birdSquish];
-    images.forEach((image) => {
-      new Image().src = image;
-    });
-  }, []);
+  // useEffect(() => {
+  //   const images = [bird, birdEureka, birdSquish];
+  //   images.forEach((image) => {
+  //     new Image().src = image;
+  //   });
+  // }, []);
 
   return (
     <StyledDiv className={props.nextTweet ? "next" : ""}>
