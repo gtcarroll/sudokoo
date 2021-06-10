dacbaccdf0c3e34a2a60fc2f4841f5af0cfe55f6
gtcarroll <gtcarroll96@gmail.com>
2021-06-09 17:36:08 -0700

    v1.373 - eliminated unused import

--- src/components/birdfeed/BirdFeed.js
+++ src/components/birdfeed/BirdFeed.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { BirdTweet } from "./";
 import { colors, animation } from "../../params.js";
